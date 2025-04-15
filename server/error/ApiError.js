1e21bb130be7fd67e636150fcd0776aa035ab5a8
sarcoma29 <soroin.georiy@yandex.ru>
2025-04-15 20:58:28 +0300

    Controllers errorApi Routers

---
+++ server/error/ApiError.js
@@ -0,0 +1,15 @@
+class ApiError extends Error() {
+    constructor (status, message){
+        super();
+        this.status = status;
+        this.message = message;
+    }
+
+    static internal(message) {
+        return new ApiError(500, message)
+    }
+
+    static forbidden(message) {
+        return new ApiError(403, message)
+    }
+}
