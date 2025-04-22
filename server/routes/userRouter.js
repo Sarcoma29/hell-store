9245343da137c1ddba7199d0ff77a1e8863dd285
sarcoma29 <soroin.georiy@yandex.ru>
2025-04-22 22:36:06 +0300

    main Backend

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
-const userController = require('../controllers/userController')
+const UserController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/registration', userController.registraion )
-router.post('/login', userController.login )
-router.get('/auth', userController.check )
+router.post('/registration', UserController.registration )
+router.post('/login', UserController.login )
+router.get('/auth', authMiddleware,  UserController.check )
 
 module.exports = router
